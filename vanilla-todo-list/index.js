19876dfd164fcf58d4a9895c4b36a5bb9ac9e0df
wei zheng <weijie0191@gmail.com>
2020-07-05 18:41:16 -0400

    init vanilla-todo-list

---
+++ vanilla-todo-list/index.js
@@ -0,0 +1,7 @@
+//script start when html dom loaded
+window.onload = function () {
+    var addButton = document.getElementById("add-button");
+    addButton.onclick = function () {
+        alert("You click add");
+    };
+};

d9b8f7ac90f121e44193decf412a8188ca6b3b92
Saee Gore <saee.works@gmail.com>
2020-07-10 00:16:54 -0400

    Update index.js

--- vanilla-todo-list/index.js
+++ vanilla-todo-list/index.js
@@ -1,6 +1,6 @@
- function addBtn () {
+function addBtn () {
     var textList = document.getElementById("input-text").value;
-        document.getElementById("list-demo").innerHTML += textList+"<button >X</button> <button>Edit</button>" +"<br>" ;
+        document.getElementById("list-demo").innerHTML += textList+"<button ><i class='fas fa-trash-alt'></i></button> <button><i class='fas fa-edit'></i></button>" +"<br>" ;
         // for clearing the input box
         document.getElementById('input-text').value = '';
     }
